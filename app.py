a28356f8d107fa76b26ac7db77fb3211c168f031
Tom Powell <zifnab@zifnab06.net>
2014-12-20 00:55:21 -0700

    Should work now
    Sleep time

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ with app.app_context():
             paste.delete()
             abort(404)
         else:
-            return render_paste(paste, paste.user == current_user.username)
+            return render_paste(paste, paste.user.username == current_user.username)
 
 
     def htmlify(string, language=None):

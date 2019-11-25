7c50b693c295ccd543d926b1ecddc6f28fc3f612
Tom Powell <tom@powell.io>
2019-11-25 13:12:29 -0800

    update config for external mongo

--- config.py
+++ config.py
@@ -2,4 +2,8 @@ import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-MONGODB_SETTINGS = { "db": os.environ.get("MONGODB_DB", "zifbin") }
+MONGODB_SETTINGS = {
+    "db": os.environ.get("MONGODB_DB", "zifbin"),
+    "host": os.environ.get("MONGODB_HOST", "localhost"),
+    "port": int(os.environ.get("MONGODB_PORT", "27017")),
+}
